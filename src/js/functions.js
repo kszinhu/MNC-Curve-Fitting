f3ebc02a3c7a67426d116408e3600afa21c12617
kszinhu <rodrigues.cassiano2007@gmail.com>
2021-08-11 15:13:37 -0300

    added functions for interface

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,3 +1,46 @@
+/**
+ * Creates table to insert the X points and their respective Y.
+ */
+const resize = () => {
+  let table = document.querySelector("#table");
+  let n = parseInt(document.querySelector("#n").value);
+  table.innerHTML = "";
+  if (isNaN(n) || n < 1) {
+    return;
+  }
+
+  let row = new Array(3);
+  let cells = new Array(3);
+
+  Array.from({ length: 3 }, (v, k) => k).forEach((i) => {
+    row[i] = document.createElement("tr");
+    cells[i] = document.createElement("th");
+  });
+
+  // Labels
+  cells[0].innerHTML = "i";
+  cells[1].innerHTML = "x";
+  cells[2].innerHTML = "y";
+
+  row.map((element, index) => {
+    element.appendChild(cells[index]);
+  });
+
+  for (let index = 0; index < n; index++) {
+    cells[0] = document.createElement("th");
+    cells[0].innerHTML = index + 1;
+    for (let j = 1; j < 3; j++) {
+      cells[j] = document.createElement("td");
+      cells[j].innerHTML = `<input type="text" id="point${index}${j}">`;
+    }
+    for (let j = 0; j < 3; j++) row[j].appendChild(cells[j]);
+  }
+  for (let index = 0; index < 3; index++) table.appendChild(row[index]);
+};
+
+/**
+ * Function to create popup for the information button.
+ */
 const infoButton = () => {
   const body = document.querySelector("body");
   const elem = document.createElement("div");
@@ -31,6 +74,3 @@ const infoButton = () => {
     </div>
     `;
 };
-
-const removeInfoButton = () =>
-  document.querySelector(".details-modal").remove();
